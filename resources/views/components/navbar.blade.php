a8071e7b4c56180715a47d18b133a439160ea743
fadhlimulyana20 <baba.fadhli@gmail.com>
2020-07-28 11:37:38 +0700

    Menambahkan pesan selamat datang pda homepage

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -13,6 +13,9 @@
                 <li class="nav-item {{ (request()->is('tentang')) ? 'active' : '' }}">
                     <a class="nav-link" href={{route('about')}}>Tentang</a>
                 </li>
+                <li class="nav-item my-auto">
+                    <a href="" class="btn btn-sm btn-block btn-outline-light rounded-pill font-weight-bold {{ (request()->is('login')) ? 'active' : '' }}">Login</a>
+                </li>
             </ul>
         </div>
     </div>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,8 +1,59 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1>Selamat Datang di Aplikasi Webinar UNS</h1>
-    <p>Aplikasi Webinar UNS adalah aplikasi yang digunakan untuk mendaftarkan diri pada webinar yang diselenggarakan oleh UNS.</p>
-</div>
+<section id="main" class="content-section">
+    <div class="container">
+        <div class="row d-flex align-items-center text-md-left text-center">
+            <div class="col-md-6">
+                <img src={{asset('image/Telecommuting-rafiki.svg')}} alt="" class="img-fluid">
+            </div>
+            <div class="col-md-6">
+                <h1>Selamat Datang di Aplikasi <span class="font-weight-bold">Webinar UNS</span> </h1>
+                <p>Aplikasi Webinar UNS adalah aplikasi yang digunakan untuk mendaftarkan diri pada webinar yang diselenggarakan oleh UNS.</p>
+                
+                <button class="btn btn-lg btn-dark font-weight-bold rounded-pill">Getting Started</button>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="content-section bg-dark">
+    <div class="container">
+        <h2 class="text-center text-white">Webinar <span class="font-weight-bold">Terbaru</span></h2>
+        <div class="row mt-5">
+            <div class="col-md-3 col-6">
+                <div class="card mb-4">
+                    <img src="https://picsum.photos/536/354" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                  </div>
+            </div>
+            <div class="col-md-3 col-6">
+                <div class="card mb-4">
+                    <img src="https://picsum.photos/536/354" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                  </div>
+            </div>
+            <div class="col-md-3 col-6">
+                <div class="card mb-4">
+                    <img src="https://picsum.photos/536/354" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                  </div>
+            </div>
+            <div class="col-md-3 col-6">
+                <div class="card mb-4">
+                    <img src="https://picsum.photos/536/354" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                  </div>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
