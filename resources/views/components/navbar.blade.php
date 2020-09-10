8bddae858b78324dc078ffb2905cd3ba05ab9b3b
fadhlimulyana20 <baba.fadhli@gmail.com>
2020-09-10 15:56:15 +0700

    Menambanhkan logo uns pada navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,9 @@
 <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light shadow">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/">Webinar <span class="font-weight-bold">UNS</span></a>
+        <a class="navbar-brand" href="/">
+            <img src={{asset('image/Logo-UNS-Biru.png')}} width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+            Webinar <span class="font-weight-bold">UNS</span>
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
